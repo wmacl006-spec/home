d7629b2b54bef5febddf6333d0493a0c0c6aac6c
William Maclean <wmacl006@gmail.com>
2026-01-12 20:04:35 -0500

    Update goaldata.js

--- goaldata.js
+++ goaldata.js
@@ -1,6 +1,15 @@
 const goalData = [
-  { date: "2026-01-01", value: 10 },
-  { date: "2026-01-02", value: 12 },
-  { date: "2026-01-03", value: 15 },
-  { date: "2026-01-04", value: 18 }
+  { date: "2026-01-01", value: 80 },
+  { date: "2026-01-02", value: 75 },
+  { date: "2026-01-03", value: 95 },
+  { date: "2026-01-04", value: 100 },
+  { date: "2026-01-05", value: 90 },
+  { date: "2026-01-06", value: 100 },
+  { date: "2026-01-07", value: 95 },
+  { date: "2026-01-08", value: 90 },
+  { date: "2026-01-09", value: 80 },
+  { date: "2026-01-10", value: 95 },
+  { date: "2026-01-11", value: 100 },
+  { date: "2026-01-12", value: 110 }
 ];
+
